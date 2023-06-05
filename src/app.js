453dedaf7e5ab55123157e17d69086549c8c4840
HieuNguyen <hieuan.mta@gmail.com>
2023-06-06 00:57:27 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const compression = require('compression');
+const express = require('express');
+const morgan = require('morgan');
+const { default: helmet } = require('helmet');
+const app = express();
+
+// init middleware
+// ghi log request: morgan('dev'), morgan('combined')
+app.use(morgan('dev'));
+// bao mat thong tin server: helmet
+app.use(helmet());
+// giam bang thong: compression
+app.use(compression());
+
+// init db
+
+// init routes
+app.get('/', (req, res, next) => {
+  const str = 'Hello toi la the nay the kia the no';
+  return res.status(200).json({
+    message: 'Welcome to backend architecture',
+    metadata: str.repeat(10000),
+  });
+});
+
+// handling error
+
+module.exports = app;
