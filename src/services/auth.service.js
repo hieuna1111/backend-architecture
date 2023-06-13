db86e98733bd0b6f7f62132b3a41a1cb66e80cb3
HieuNguyen <hieuan.mta@gmail.com>
2023-06-14 00:17:41 +0700

    refactor: handle throw error and success response

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -7,7 +7,10 @@ const pick = require('../common/utils/pick.util');
 const TokenService = require('./token.service');
 const userModel = require('../models/user.model');
 const { USER_ROLE } = require('../common/constant/auth.constant');
-const { throwBadRequest } = require('../common/utils/handleError.util');
+const {
+  throwBadRequest,
+  databaseError,
+} = require('../common/utils/handleError.util');
 
 class AuthService {
   static signUp = async ({ name, email, password }) => {
@@ -23,46 +26,40 @@ class AuthService {
       password: passwordHash,
       roles: [USER_ROLE.RESTAURANT_OWNER],
     });
+    databaseError(!newUser, 'New user not registered yet!');
 
-    if (newUser) {
-      // created privateKey, publicKey
-      const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
-        modulusLength: 4096,
-        publicKeyEncoding: {
-          type: 'pkcs1', // Public Key CryptoGraphic KeyStore
-          format: 'pem',
-        },
-        privateKeyEncoding: {
-          type: 'pkcs1', // Public Key CryptoGraphic KeyStore
-          format: 'pem',
-        },
-      });
+    // created privateKey, publicKey
+    const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
+      modulusLength: 4096,
+      publicKeyEncoding: {
+        type: 'pkcs1', // Public Key CryptoGraphic KeyStore
+        format: 'pem',
+      },
+      privateKeyEncoding: {
+        type: 'pkcs1', // Public Key CryptoGraphic KeyStore
+        format: 'pem',
+      },
+    });
 
-      // created token pair
-      const { accessToken, refreshToken } = await createTokenPair({
-        payload: { userId: newUser._id, email },
-        publicKey,
-        privateKey,
-      });
+    // created token pair
+    const { accessToken, refreshToken } = await createTokenPair({
+      payload: { userId: newUser._id, email },
+      publicKey,
+      privateKey,
+    });
 
-      const publicKeyString = await TokenService.saveToken({
-        method: 'create',
-        userId: newUser._id,
-        publicKey,
-        refreshToken,
-      });
-      throwBadRequest(!publicKeyString, 'Error, save token!');
-
-      return {
-        user: pick(newUser, ['_id', 'name', 'email']),
-        accessToken,
-        refreshToken,
-      };
-    }
+    const publicKeyString = await TokenService.saveToken({
+      method: 'create',
+      userId: newUser._id,
+      publicKey,
+      refreshToken,
+    });
+    throwBadRequest(!publicKeyString, 'Error, save token!');
 
     return {
-      code: 200,
-      metadata: null,
+      user: pick(newUser, ['_id', 'name', 'email']),
+      accessToken,
+      refreshToken,
     };
   };
 }
