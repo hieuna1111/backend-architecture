5e848777d489b3126f13dab3ee97d8a2645006de
HieuNguyen <hieuan.mta@gmail.com>
2023-06-17 19:42:20 +0700

    refactor: logout response

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,18 +1,18 @@
 'use strict';
 
 const express = require('express');
-const accessController = require('../../controllers/user.controller');
+const authController = require('../../controllers/auth.controller');
 const { authentication } = require('../../common/utils/auth.util');
 const router = express.Router();
 
 // sign up
-router.post('/auth/sign-up', accessController.signUp);
+router.post('/auth/sign-up', authController.signUp);
 // sing in
-router.post('/auth/login', accessController.login);
+router.post('/auth/login', authController.login);
 
 // authentication
 router.use(authentication);
 // check authentication after access logout
-router.post('/auth/logout', accessController.logout);
+router.post('/auth/logout', authController.logout);
 
 module.exports = router;
